6371ae0d813aa6e784274587ef1319865315782c
Piyush Garg <piyushgarg.dev@gmail.com>
2024-10-01 14:10:35 +0530

    render editor js dynamically with ssr false

--- app/admin/blog/new/page.tsx
+++ app/admin/blog/new/page.tsx
@@ -1,4 +1,8 @@
-import CreateBlogForm from "./_components/create-blog";
+import dynamic from "next/dynamic";
+
+const CreateBlogForm = dynamic(() => import("./_components/create-blog"), {
+  ssr: false,
+});
 
 export default function CreateBlogPage() {
   return <CreateBlogForm />;
